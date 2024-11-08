20607f3d21e7921a93d04153b84926819957f9a2
Mia Ameisbichler <152974150+Mia-Ameisbichler@users.noreply.github.com>
2024-11-08 14:00:19 +0100

    first commit

---
+++ TrackMe/TrackMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackMe
+//
+//  Created by Mia Ameisbichler on 08.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackMe/TrackMe/TrackMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackMeApp.swift
+//  TrackMe
+//
+//  Created by Mia Ameisbichler on 08.11.24.
+//
+
+import SwiftUI
+
+@main
+struct TrackMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
